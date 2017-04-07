da818c6e47b79ba986a7982179c39b3b94951a03
kagos <kristin.agos@yahoo.com>
2017-04-07 02:26:16 -0500

    Basic React app - WORKING - with webpack, React, Babel, and Less configured.

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>I work!</h1>
+      </div>);
+  }
+}
